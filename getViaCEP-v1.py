9a207afc195b234100ea0ddea1ad7a1ac72ca189
thimasan <thyagus.santana@gmail.com>
2025-06-12 20:29:01 -0300

    upload via cep

---
+++ getViaCEP-v1.py
@@ -0,0 +1,15 @@
+import socket
+
+HOST = 'viacep.com.br'
+PORT = 80
+CEP = "59064250"
+
+sockTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sockTCP.connect((HOST,PORT))
+
+sockTCP.send(("GET"+CEP+"/json/HTTP/1.1\r\n"+"HOST:"+HOST+"\r\n"+"\r\n").encode("utf-8"))
+data = sockTCP.recv(4096)
+
+print(data.decode("utf-8"))
+
+sockTCP.close()
